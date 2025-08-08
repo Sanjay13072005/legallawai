f67f4c1dad378e38d00bed829a6432eb21b8070c
Sanjay B <151824779+Sanjay13072005@users.noreply.github.com>
2025-08-08 14:51:37 +0530

    Add files via upload
    gg py file uploaded'

---
+++ gg.py
@@ -0,0 +1,10 @@
+score=int(input("enter a marks:"))
+if (score>=85):
+    print("outstanding students")
+elif (55>=score<=84):
+    print("averaging students")
+elif (score<=55):
+    print("poor student")
+else:
+    print("invalid")
+
